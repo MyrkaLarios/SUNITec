a5a451543bb73aa3e05fc08a8e4663612f926ab6
Myrka Larios <myrka@michelada.io>
2018-08-02 16:19:26 -0500

    Makes the connection to the DB

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,18 @@ var knex = require('../knex');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  knex('tableName').insert({name: 'Myrka'});
-  res.render('index', { title: 'Express' });
+  knex('NIVEL').select().then(data => {
+    console.log(data);
+    res.json(data)
+  })
 });
+
+router.get('/insert/:id', (req, res, next) => {
+  knex('NIVEL').insert({
+    id_nivel: req.params.id
+  }).then(data => {
+    res.json(data)
+  })
+})
 
 module.exports = router;
