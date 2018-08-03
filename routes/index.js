91b6902de97e44f529b4dd2379f3ed5701d386a7
Myrka Larios <myrka@michelada.io>
2018-08-03 00:02:56 -0500

    WIP sending posts to the DB

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,33 @@ var knex = require('../knex');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  knex('NIVEL').select().then(data => {
-    console.log(data);
-    res.json(data)
-  })
+  // knex('NIVEL').select().then(data => {
+  //   console.log(data);
+  //   res.json(data)
+  // })
+  res.render('index')
 });
 
-router.get('/insert/:id', (req, res, next) => {
-  knex('NIVEL').insert({
-    id_nivel: req.params.id
+router.post('/insert/question', (req, res, next) => {
+  knex('PREGUNTA').insert({
+    id_pregunta: null,
+    descripcion: req.body.descripcion,
+    id_unidad: req.body.id_unidad,
+    valor: req.body.valor,
+    id_coordi: req.body.id_coordi
   }).then(data => {
-    res.json(data)
+    // knex.insert([
+    //   {
+    //     id_opciones: null,
+    //     contenido: req.body.option1,
+    //     id_pregunta: data[0],
+    //     resp_correcta:
+    //   },
+    //   {
+    //     title: 'Fahrenheit 451'
+    //   }], 'id').into('books')
+    console.log(data[0])
+    console.log(req.body)
   })
 })
 
